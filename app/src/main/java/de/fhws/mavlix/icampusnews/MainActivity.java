190d3c7400a7959b76705a27a29fdffc90980c98
Marvin Vierling <marvinvierling@hotmail.de>
2016-01-10 14:01:27 +0100

    Just a test with a fragment

--- app/src/main/java/de/fhws/mavlix/icampusnews/MainActivity.java
+++ app/src/main/java/de/fhws/mavlix/icampusnews/MainActivity.java
@@ -1,13 +1,23 @@
 package de.fhws.mavlix.icampusnews;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment testFragment = new TestFragment();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.mainFrameLayout,testFragment);
+        ft.commit();
+
     }
 }
---
+++ app/src/main/java/de/fhws/mavlix/icampusnews/TestFragment.java
@@ -0,0 +1,19 @@
+package de.fhws.mavlix.icampusnews;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Marvin on 10.01.2016.
+ */
+public class TestFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_test,container,false);
+    }
+}
